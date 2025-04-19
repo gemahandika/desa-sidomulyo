319c78c57fb0df5928a69563f2dc8399cac12261
gema_handika <gemahandika88@gmail.com>
2025-04-19 12:06:42 +0700

    update

--- dash-app/app/config/koneksi.php
+++ dash-app/app/config/koneksi.php
@@ -1,12 +1,12 @@
 <?php
 // koneksi local
-$koneksi = mysqli_connect('localhost', 'root', '', 'db_desa');
-if (mysqli_connect_errno()) {
-    echo mysqli_connect_error();
-}
-
-// // koneksi public
-// $koneksi = mysqli_connect('localhost', 'jnee6778_mesit', 'Jnemes2017', 'jnee6778_dbdesa');
+// $koneksi = mysqli_connect('localhost', 'root', '', 'db_desa');
 // if (mysqli_connect_errno()) {
 //     echo mysqli_connect_error();
 // }
+
+// // koneksi public
+$koneksi = mysqli_connect('localhost', 'jnee6778_mesit', 'Jnemes2017', 'jnee6778_dbdesa');
+if (mysqli_connect_errno()) {
+    echo mysqli_connect_error();
+}
